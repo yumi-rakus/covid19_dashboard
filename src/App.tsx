c7ffcaeec8723b07fb38852a5ca05dff2eeec748
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 18:51:45 +0900

    Cardsコンポーネントの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import './assets/css/App.css';
+import Cards from "./components/covid/Cards";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Cards/>
+        </div>
+    );
 }
 
 export default App;
