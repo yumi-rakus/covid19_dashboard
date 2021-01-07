6f7d8022813b7fce29e887e953e6cd3ffa595ef2
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 18:50:07 +0900

    Cardsコンポーネントの実装

--- src/components/covid/Cards.tsx
+++ src/components/covid/Cards.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import styles from '../../assets/css/Cards.module.css'
+
+import Card from "./Card";
+import {Card as MaterialCard, Grid} from "@material-ui/core";
+
+import {useSelector} from "react-redux";
+import {selectDataDaily} from "../../store/slices/covidSlice";
+
 
 const Cards: React.FC = () => {
-    return (
-        <div>
 
+    // storeのstateを取得
+    const dataDaily = useSelector(selectDataDaily);
+
+    return (
+        <div className={styles.container}>
+            <Grid container spacing={1} justify={"center"}>
+                <Grid item xs={12} md={3} component={MaterialCard} className={styles.infected}>
+                    <Card item={'infected'} title={'Infected person'}
+                          numberOfPeople={dataDaily[dataDaily.length - 1].Confirmed}/>
+                </Grid>
+                <Grid item xs={12} md={3} component={MaterialCard} className={styles.recovered}>
+                    <Card item={'recovered'} title={'Recovered person'}
+                          numberOfPeople={dataDaily[dataDaily.length - 1].Recovered}/>
+                </Grid>
+                <Grid item xs={12} md={3} component={MaterialCard} className={styles.deaths}>
+                    <Card item={'deaths'} title={'Deaths person'}
+                          numberOfPeople={dataDaily[dataDaily.length - 1].Deaths}/>
+                </Grid>
+            </Grid>
         </div>
     );
 };
