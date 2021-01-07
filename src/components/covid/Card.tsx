23e4c5dad6dfd72c4589c2ae5e97da13b980bbbd
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 18:49:41 +0900

    Cardコンポーネントの実装

--- src/components/covid/Card.tsx
+++ src/components/covid/Card.tsx
@@ -1,10 +1,45 @@
 import React from "react";
 
-const Card: React.FC = () => {
-    return (
-       <div>
+import CountUp from "react-countup";
+import {CardContent, Typography} from "@material-ui/core";
 
-       </div>
+import {GiHastyGrave, MdLocalHospital, AiFillLike} from "react-icons/all";
+
+// Propsの定義
+interface Props {
+    item: string;
+    title: string;
+    numberOfPeople: number;
+}
+
+const Card: React.FC<Props> = props => {
+
+    // 項目によってアイコンのJSXを決定する
+    let icon;
+
+    if (props.item === 'infected') {
+        icon = <MdLocalHospital/>;
+    } else if (props.item === 'recovered') {
+        icon = <AiFillLike/>;
+    } else {
+        icon = <GiHastyGrave/>;
+    }
+
+    return (
+        <CardContent>
+            <Typography color={"textSecondary"} gutterBottom>
+                {icon}
+                {props.title}
+            </Typography>
+            <Typography variant={"h6"}>
+                <CountUp
+                    start={0}
+                    end={props.numberOfPeople}
+                    duration={1.5}
+                    separator={','}
+                />
+            </Typography>
+        </CardContent>
     );
 };
 
