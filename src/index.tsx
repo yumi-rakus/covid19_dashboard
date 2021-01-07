a52f40a28251b07e8c1ba57279036bd88cb28b60
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 16:59:54 +0900

    ストアをアプリ内で利用するため、react-redux から提供される Provider コンポーネントを使用

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from './store/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // ストアをアプリ内で利用するため、react-redux から提供される Provider コンポーネントを使用
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
