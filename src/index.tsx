9bf2ce49dd4114cbf3645e6a3b4323ea9d59e4df
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 17:00:42 +0900

    import文を修正

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {store} from './store/index';
+import {store} from './store';
 
 ReactDOM.render(
     // ストアをアプリ内で利用するため、react-redux から提供される Provider コンポーネントを使用
