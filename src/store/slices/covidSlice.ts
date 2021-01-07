c264decb7a2f0e92ddb6cc21bd9df1334157f578
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 15:41:23 +0900

    非同期関数fetchDataDailyを定義

--- src/store/slices/covidSlice.ts
+++ src/store/slices/covidSlice.ts
@@ -1,10 +1,10 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
 import dataDailyJson from '../../types/apiDataDaily.json'
 
 // ベースのURL
-const apiUrl = 'https://api.covid19api.com/total/country/';
+const apiUrl = 'https://api.covid19api.com/total/country';
 
 // APIから取得するdataをtypeとして定義
 type DATA_DAILY = typeof dataDailyJson;
@@ -20,6 +20,13 @@ const initialState: covidState = {
     dataDaily: dataDailyJson,
     country: 'Japan'
 };
+
+// createAsyncThunk: 非同期に対応したAction Creator
+export const fetchDataDaily = createAsyncThunk('covid/getDaily', async (country: string) => {
+    // GenericsでAPIから取得するデータ型を保証
+    const {data} = await axios.get<DATA_DAILY>(`${apiUrl}/${country}`);
+    return {data: data, country: country};
+})
 
 
 const covidSlice = createSlice({
