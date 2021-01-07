e20b78c7197ecf73c85f031e5388c1d47f0258b9
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 15:20:46 +0900

    stateのtype, 初期値を設定

--- src/store/slices/covidSlice.ts
+++ src/store/slices/covidSlice.ts
@@ -1,13 +1,31 @@
 import {createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+
+import dataDailyJson from '../../types/apiDataDaily.json'
+
+// ベースのURL
+const apiUrl = 'https://api.covid19api.com/total/country/';
+
+// APIから取得するdataをtypeとして定義
+type DATA_DAILY = typeof dataDailyJson;
+
+// state
+type covidState = {
+    dataDaily: DATA_DAILY;
+    country: string;
+};
+
+// stateの初期値
+const initialState: covidState = {
+    dataDaily: dataDailyJson,
+    country: 'Japan'
+};
+
 
 const covidSlice = createSlice({
     name: 'covid',
-    initialState: {
-
-    },
-    reducers: {
-
-    }
+    initialState: initialState,
+    reducers: {}
 });
 
 export default covidSlice.reducer;
