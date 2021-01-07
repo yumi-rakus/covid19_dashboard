ab2a7d22c43a628e4bb9d66d390da3175adb1cac
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-07 14:56:34 +0900

    useSelectorを宣言

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {useSelector as rawUseSelector, TypedUseSelectorHook} from "react-redux";
 
 export const store = configureStore({
     reducer: {
@@ -8,3 +9,6 @@ export const store = configureStore({
 
 // 複数のreducerをまとめた場合に、最終的なStateの型を取り出す（今回はreducerは１つだが）
 export type RootState = ReturnType<typeof store.getState>;
+
+// 型情報付きのuseSelectorをここで宣言
+export const useSelector: TypedUseSelectorHook<RootState> = rawUseSelector;
