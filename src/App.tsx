fee87f3c6863ee7e2bc5494b9459281d133cedb1
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-08 11:09:44 +0900

    PieChartコンポーネントの表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './assets/css/App.css';
 import Cards from "./components/covid/Cards";
 import Chart from "./components/covid/Chart";
+import PieChart from "./components/covid/PieChart";
 
 function App() {
     return (
         <div>
             <Cards/>
             <Chart/>
+            <PieChart/>
         </div>
     );
 }
