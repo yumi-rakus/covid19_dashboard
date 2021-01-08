b9e0f0dfe65e3b1fbfc09cc4b0eb687f8bbfc009
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-08 11:30:24 +0900

    SwitchCountry.tsxの実装

--- src/components/covid/SwitchCountry.tsx
+++ src/components/covid/SwitchCountry.tsx
@@ -1,10 +1,58 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
+
+import {makeStyles, NativeSelect, FormControl} from "@material-ui/core";
+
+import {useDispatch} from "react-redux";
+import {fetchDataDaily} from "../../store/slices/covidSlice";
+
+const useStyle = makeStyles((theme) => ({
+    formControl: {
+        marginBottom: theme.spacing(3),
+        minWidth: 320
+    }
+}));
 
 const SwitchCountry: React.FC = () => {
-    return (
-        <div>
 
-        </div>
+    // 使用準備
+    const classes = useStyle();
+    const dispatch = useDispatch();
+
+    // 国リスト
+    const countries = [
+        'japan',
+        'china',
+        'us',
+        'france',
+        'italy',
+        'spain',
+        'united kingdom',
+        'germany',
+        'russia',
+        'brazil',
+        'taiwan',
+        'thailand',
+        'sweden',
+        'india'
+    ];
+
+    // event handler
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>): void => {
+        // 非同期関数の呼び出し
+        // 国が変更される度にAPIを呼び出し、stateのデータを更新する
+        dispatch(fetchDataDaily(e.target.value));
+    }
+
+    return (
+        <FormControl className={classes.formControl}>
+            <NativeSelect onChange={handleChange}>
+                {countries.map((country, i) => (
+                    <option key={i} value={country}>
+                        {country}
+                    </option>
+                ))}
+            </NativeSelect>
+        </FormControl>
     );
 };
 
