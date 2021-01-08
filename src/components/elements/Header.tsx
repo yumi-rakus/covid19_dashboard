48e2ff90eef7c9bce319786101ef7b84fc24204d
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-08 12:50:35 +0900

    ・Header.tsx実装 ・updateDateの表示切替を実装

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -1,10 +1,40 @@
 import React from "react";
+import {makeStyles, AppBar, Toolbar, Typography} from "@material-ui/core";
 
-const Header: React.FC = () => {
+import {useSelector} from "react-redux";
+import {selectIsLoading} from "../../store/slices/covidSlice";
+
+interface Props {
+    latestDate: string
+}
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1
+    }
+}))
+
+const Header: React.FC<Props> = props => {
+
+    // 使用準備
+    const classes = useStyles();
+
+    // store/stateからデータを取得
+    const isLoading = useSelector(selectIsLoading);
+
     return (
-        <div>
-
-        </div>
+        <AppBar position={"absolute"}>
+            <Toolbar>
+                <Typography variant={"h6"} className={classes.title}>
+                    COVID 19 Live DashBoard
+                </Typography>
+                <div>
+                    <Typography variant={"body1"}>
+                        {isLoading ? '' : 'UpdateDate: ' + props.latestDate}
+                    </Typography>
+                </div>
+            </Toolbar>
+        </AppBar>
     );
 };
 
