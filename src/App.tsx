56915b3d79be6fee2337ec779944f5321674c2e2
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-08 10:48:17 +0900

    Chartコンポーネントの表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/css/App.css';
 import Cards from "./components/covid/Cards";
+import Chart from "./components/covid/Chart";
 
 function App() {
     return (
         <div>
             <Cards/>
+            <Chart/>
         </div>
     );
 }
