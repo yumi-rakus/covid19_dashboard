cb6cfc6e9dbe4a3a940954cecf39aed944be62f3
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-08 11:30:01 +0900

    SwitchCountryコンポーネントの表示

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './assets/css/App.css';
 import Cards from "./components/covid/Cards";
 import Chart from "./components/covid/Chart";
 import PieChart from "./components/covid/PieChart";
+import SwitchCountry from "./components/covid/SwitchCountry";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Cards/>
             <Chart/>
             <PieChart/>
+            <SwitchCountry/>
         </div>
     );
 }
